eb1f72f3d4a32d6510c6a43c96632d1755a177e9
tomasmendes <mendes9@gmail.com>
2014-05-12 01:24:54 +0200

    Plot 3 added

---
+++ plot3.R
@@ -0,0 +1,21 @@
+f <- read.table("household_power_consumption.txt", na.strings="?", sep=";", header=TRUE)
+
+f <- transform(f, Date = as.Date(Date, format = "%d/%m/%Y"))
+f <- transform(f, Time = strptime(Time, format = "%H:%M:%S"))
+f$concat <- paste(f$Date, f$Time,sep=" ")
+
+png(filename="plot3.png", width = 480, height = 480)
+min <- as.Date("01/02/2007", format = "%d/%m/%Y")
+max <- as.Date("02/02/2007", format = "%d/%m/%Y")
+
+fsub <- subset(f, Date >= min)
+fsub <- subset(fsub, Date <= max)
+
+plot(1:nrow(fsub), fsub$Sub_metering_1, type="l", ylab="Energy sub metering", xlab="")
+lines(1:nrow(fsub), fsub$Sub_metering_2,col="red")
+lines(1:nrow(fsub), fsub$Sub_metering_3,col="blue")
+legend("topright", pch="-", col=c("black","red","blue"),
+       legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+
+
+dev.off()
