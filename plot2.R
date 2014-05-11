53754793363e30c161575a2db1501760aa279023
tomasmendes <mendes9@gmail.com>
2014-05-12 01:13:35 +0200

    Plot 2 added

--- plot2.R
+++ plot2.R
@@ -1,13 +1,14 @@
 f <- read.table("household_power_consumption.txt", na.strings="?", sep=";", header=TRUE)
+
 f <- transform(f, Date = as.Date(Date, format = "%d/%m/%Y"))
 f <- transform(f, Time = strptime(Time, format = "%H:%M:%S"))
+f$concat <- paste(f$Date, f$Time,sep=" ")
 
-png(filename="plot1.png", width = 480, height = 480)
+png(filename="plot2.png", width = 480, height = 480)
 min <- as.Date("01/02/2007", format = "%d/%m/%Y")
 max <- as.Date("02/02/2007", format = "%d/%m/%Y")
 
 fsub <- subset(f, Date >= min)
 fsub <- subset(fsub, Date <= max)
-hist(fsub$Global_active_power, main="Global Active Power",
-      xlab="Global Active Power (kilowatts)", col="red", xlim=range(0:6))
+plot(1:nrow(fsub), fsub$Global_active_power, type="l", ylab="Global Active Power (kilowatts)", xlab="")
 dev.off()
